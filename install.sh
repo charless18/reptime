62a65007ab034e864011afc2256cdfcd510017b4
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-17 15:35:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 echo "V2ray_To_Replit 一键搭建脚本"
 echo "脚本作者：ok8023"
 echo "开源地址：https://github.com/ok8023/V2ray_To_Replit/"
-git clone https://github.com/ok8023/V2ray_To_Replit && mv -b V2ray_To_Replit/files/* ./ && mv -b V2ray_To_Replit/files/.[^.]* ./ && rm -rf *~ && rm -rf V2ray_To_Replit
+nix-env -iA nixpkgs.wget
+wget https://raw.githubusercontent.com/ok8023/V2ray_To_Replit/main/files/Replit-v2ray-main.tar.gz
+tar -zxvf Replit-v2ray-main.tar.gz
 echo "恭喜项目搭建完成"
 echo "点击Run立即运行项目"
-echo "成功部署后注释掉main.sh第一行的 git clone"
-echo "示例：#git clone https://github.com/louislam/uptime-kuma.git"
+echo "查询用户名及密码运行下面命令"
+
