0a0276ff5f6f0a6d6f8e409c9756801aefe4d778
charless18 <131284488+charless18@users.noreply.github.com>
2023-04-19 10:34:09 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-echo "V2ray_To_Replit 一键搭建脚本"
-echo "脚本作者：ok8023"
-echo "开源地址：https://github.com/ok8023/V2ray_To_Replit/"
+echo "Hello"
+echo "脚本作者：ok023"
+echo "开源地址：https://github.com/charless18/reptime/"
 nix-env -iA nixpkgs.wget
-wget -O .replit https://raw.githubusercontent.com/ok8023/V2ray_To_Replit/main/files/.replit
-wget -O CNAME https://raw.githubusercontent.com/ok8023/V2ray_To_Replit/main/files/CNAME
-wget -O README.md https://raw.githubusercontent.com/ok8023/V2ray_To_Replit/main/files/README.md
-wget -O main.sh https://raw.githubusercontent.com/ok8023/V2ray_To_Replit/main/files/main.sh
-wget -O replit.nix https://raw.githubusercontent.com/ok8023/V2ray_To_Replit/main/files/replit.nix
+wget -O .replit https://raw.githubusercontent.com/charless18/reptime/main/files/.replit
+wget -O CNAME https://raw.githubusercontent.com/charless18/reptime/main/files/CNAME
+wget -O README.md https://raw.githubusercontent.com/charless18/reptime/main/files/README.md
+wget -O main.sh https://raw.githubusercontent.com/charless18/reptime/main/files/main.sh
+wget -O replit.nix https://raw.githubusercontent.com/charless18/reptime/main/files/replit.nix
 echo "恭喜项目搭建完成"
 echo "点击Run立即运行项目"
 echo "服务器地址： https://项目名.用户名.repl.co"
